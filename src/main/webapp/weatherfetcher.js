3ebf5aee5c39e2aa5d11ed54c07d2ccf5d789cae
Eerik Mägi <eerik46@gmail.com>
2017-11-17 01:37:13 +0200

    Add basic error handling

--- src/main/webapp/weatherfetcher.js
+++ src/main/webapp/weatherfetcher.js
@@ -2,81 +2,91 @@ const WeatherFetcher = {};
 WeatherFetcher.stations = null;
 
 WeatherFetcher.$loading = document.getElementById('loading');
+WeatherFetcher.$unexpectedError = document.getElementById('unexpectedError');
 
 WeatherFetcher.$search = document.getElementById('search');
 WeatherFetcher.$stationsList = document.getElementById('stations');
 WeatherFetcher.$searchInput = WeatherFetcher.$search.querySelector('input[name=station]');
 WeatherFetcher.$searchForm = document.getElementById('searchForm');
+WeatherFetcher.$unknownStation = document.getElementById('unknownStation');
 WeatherFetcher.$weatherReport = document.getElementById('weatherReport');
 
 WeatherFetcher.$weatherReportTemplate = document.getElementById('template_weatherReport');
 WeatherFetcher.weatherReportTemplate =
 	Handlebars.compile(WeatherFetcher.$weatherReportTemplate.innerHTML);
 
-WeatherFetcher.init = function init() {
-	request('api/stations', {
-		method: 'GET'
-	}).then((data) => {
-		WeatherFetcher.stations = data.stations;
+WeatherFetcher.init = async function init() {
+	try {
+		const response = await request('api/stations', {
+			method: 'GET'
+		})
+		
+		WeatherFetcher.stations = response.stations;
 		
 		WeatherFetcher.stations.forEach(s => {
 			const $option = document.createElement('option');
 			$option.value = s.name;
 			WeatherFetcher.$stationsList.append($option);
 		});
-		
+	
+		WeatherFetcher.$searchForm.addEventListener('submit', WeatherFetcher.search);
 		WeatherFetcher.$loading.classList.add('hidden');
 		WeatherFetcher.$search.classList.remove('hidden');
-	});
+	} catch (e) {
+		WeatherFetcher.$unexpectedError.classList.remove('hidden');
+	}
 	
-	WeatherFetcher.$searchForm.addEventListener('submit', WeatherFetcher.search);
 };
 
-WeatherFetcher.search = function search(event) {
+WeatherFetcher.search = async function search(event) {
 	event.preventDefault();
-	
+
+	WeatherFetcher.$unknownStation.classList.add('hidden');
+	WeatherFetcher.$unexpectedError.classList.add('hidden');
 	WeatherFetcher.$weatherReport.innerHTML = '';
 	
 	const value = WeatherFetcher.$searchInput.value;
-	if (!value || !value.length) {
+	if (!value || !value.trim().length) {
 		return;
 	}
 	
 	const headers = new Headers();
 	headers.append('Content-Type', 'application/json');
 	
-	request('api/stations', {
-		method: 'POST',
-		body: JSON.stringify({
-			name: value
-		}),
-		headers: headers
-	}).then((data) => {
+	try {
+		const response = await request('api/stations', {
+			method: 'POST',
+			body: JSON.stringify({
+				name: value.trim()
+			}),
+			headers: headers
+		});
+		
 		WeatherFetcher.$weatherReport.innerHTML =
-			WeatherFetcher.weatherReportTemplate(data.station);
-	}).catch(() => {
-		console.error('Hello!!!');
-	});
+			WeatherFetcher.weatherReportTemplate(response.station);
+	} catch (e) {
+		if (e.response && e.response.status === 404) {
+			WeatherFetcher.$unknownStation.querySelector('.stationName').innerText = value;
+			WeatherFetcher.$unknownStation.classList.remove('hidden');
+		} else {
+			WeatherFetcher.$unexpectedError.classList.remove('hidden');
+		}
+	};
 };
 
 window.addEventListener("load", WeatherFetcher.init);
 
 // wrap request to react to error
-const request = (...args) => {
-	const promise = fetch(...args)
-		.then((response) => response.json())
-		.then(({success, ...rest}) => {
-			if (!success) {
-				console.error('fail!');
-				throw new Error();
-			}
-			
-			return rest;
-		});
+const request = async (...args) => {
+	const response = await fetch(...args);
+	const json = await response.json();
+	const {success, ...data} = json;
 	
-	promise.catch(() => {
-		throw new Error();
-	});
+	if (!success) {
+		throw {
+			response
+		};
+	}
 	
-	return promise;
+	return data;
 };
